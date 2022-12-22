fb347ab96558b842f78f9e19c964f487efdd93fc
Tutosleep <fbobllar@gmail.com>
2022-12-21 21:28:17 -0300

    se incorporo animacion al header

--- DesafioR1/src/components/Header.jsx
+++ DesafioR1/src/components/Header.jsx
@@ -1,6 +1,6 @@
 const Header = (props) => {
   return (
-    <nav className="text-center text-white p-3 rounded-bottom">
+    <nav className="text-center text-white p-3 rounded-bottom animate__animated animate__fadeInLeftBig">
       {props.title}
     </nav>
   );
