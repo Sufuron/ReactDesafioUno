47a75fec70d49755d2a3faa5abcff9f33a048478
Fabian Bobadilla Ruiz <35155385+Sufuron@users.noreply.github.com>
2022-12-22 00:26:13 -0300

    Add files via upload

--- DesafioR1/src/components/Footer.jsx
+++ DesafioR1/src/components/Footer.jsx
@@ -1,8 +1,37 @@
 const Footer = (props) => {
   return (
     <>
-      <footer className="text-white p-3 text-center rounded-top mt-3">
-      {props.footertitle}
+      <footer className="text-white p-3 text-center rounded-top mt-2">
+        {props.footertitle} <br />
+        <div className="text-center">
+          <button className="btn btn-outline-secondary m-2 text-decoration-none text-white">
+            <a
+              className="text-decoration-none text-white"
+              href="https://www.zooplus.es/magazine/aves/razas-de-pajaros"
+              target="_blank"
+            >
+              Pajaros
+            </a>
+          </button>
+          <button className="btn btn-outline-secondary m-2 text-decoration-none text-white">
+            <a
+              className="text-decoration-none text-white"
+              href="https://www.hogarmania.com/mascotas/perros/razas/"
+              target="_blank"
+            >
+              Perros
+            </a>
+          </button>
+          <button className="btn btn-outline-secondary m-2">
+            <a
+              className="text-decoration-none text-white"
+              href="https://tiposdeaves.com/tipos-de-cisnes/"
+              target="_blank"
+            >
+              Cisnes
+            </a>
+          </button>
+        </div>
       </footer>
     </>
   );
