f36d711beb87ff0607452eafa0028500bb5c8546
Tutosleep <fbobllar@gmail.com>
2022-12-26 23:08:04 -0300

    se creo counter jsx

---
+++ DesafioR2/src/components/Button.jsx
@@ -0,0 +1,5 @@
+const Button = (props) => {
+  return <button onClick={appearOnClick}>Submit</button>;
+};
+
+export default Button;
--- DesafioR2/src/components/Counter.jsx
+++ DesafioR2/src/components/Counter.jsx
@@ -1,13 +1,81 @@
-const Counter = (props) => {
+import { useState } from "react";
+
+const Counter = () => {
+  console.log(useState(100));
+
+  const userCredential = {
+    email: "test@test.com",
+    password: "252525",
+  };
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [welcome, setWelcome] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setError("");
+    setWelcome("");
+
+    if (!email.trim() || !password.trim()) {
+      return setError("Llena todos los campos!");
+    }
+
+    if (
+      userCredential.email === email &&
+      userCredential.password === password
+    ) {
+      return setWelcome("Bienvenido!");
+    } else {
+      return setError("Credenciales no incorrectas!");
+    }
+  };
+
   return (
     <>
-      <form
-        className="text-center p-5 m-5 rounded bgdegre text-white"
-        action="
-        "
-      >
-        hello
-      </form>
+      <div className="text-center p-5 m-5 rounded bgdegre text-white animate__animated animate__bounceIn">
+        <form onSubmit={handleSubmit}>
+          
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            className="mb-1 rounded"
+            type="text"
+            placeholder="Ingresar Username"
+            name="email"
+          />
+
+          <br />
+
+          <input
+            onChange={(e) => setPassword(e.target.value)}
+            className="rounded"
+            type="password"
+            placeholder="Ingresar Contraseña"
+            name="password"
+          />
+
+          <br />
+
+          <button
+            className="btn btn-outline-secondary mt-3 text-decoration-none text-white"
+            type="submit"
+            disable="true"
+          >
+            Submit
+          </button>
+        </form>
+
+        <div className="text-danger rounded fs-4">
+          {error ? error : null}
+          </div>
+
+        <div className="fs-4 fw-bold text-success">
+          {welcome ? welcome : null}
+        </div>
+
+      </div>
     </>
   );
 };
