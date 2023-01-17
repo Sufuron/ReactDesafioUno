aa93ccbfb44e803be37af3373a01868ce76c54d6
Tutosleep <fbobllar@gmail.com>
2023-01-17 02:54:02 -0300

    se elimino codigo de App.jsx

--- DesafioR4/src/App.jsx
+++ DesafioR4/src/App.jsx
@@ -7,18 +7,15 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <div className="row">
-          <div className="col-12 d-flex justify-content-center">
-            <Nav />
-          </div>
-          <div className="col-12">
-            <div className="d-flex justify-content-center">
-              <h1 className="text-center animate__animated animate__fadeInLeftBig rounded bg-text w-25 text-white mt-2">Pokemon List</h1>
-            </div>
-            
-            <PokemonList />
-          </div>
+        <div className="col-12 d-flex justify-content-center">
+          <Nav />
         </div>
+        <div className="d-flex justify-content-center">
+          <h1 className="text-center animate__animated animate__fadeInLeftBig rounded bg-text w-25 text-white mt-2">
+            Pokemon List
+          </h1>
+        </div>
+        <PokemonList />
       </div>
       <Footer />
     </div>
